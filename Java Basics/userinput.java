e0ddeea2e24c96c90f3924a680498feef822a3d3
Gurchetan <gurchetan9966@gmail.com>
2025-03-19 15:04:42 +0530

    commit and push

--- Java Basics/userinput.java
+++ Java Basics/userinput.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class userinput {
     public static void main(String[] args) {
         System.out.print("What's your name?");
-        Scanner input = new Scanner(System.in);
-String name = input.nextLine();
-        System.out.println("Get a Life " + name);
-        input.close();
+        try (Scanner input = new Scanner(System.in)) {
+            String name = input.nextLine();
+            System.out.println("Get a Life " + name);
+        }
     }
 }
