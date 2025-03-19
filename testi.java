47942c237862e392ccab9443c7ecede6c62611f3
Gurchetan <gurchetan9966@gmail.com>
2025-03-19 09:13:11 +0530

    commit and push

--- testi.java
+++ testi.java
@@ -1,5 +1,6 @@
-public class HelloWorld {
+public class testi{
     public static void main(String[] args) {
         System.out.println("Hello, GitHub! This is my first Java commit.");
     }
 }
+ 
