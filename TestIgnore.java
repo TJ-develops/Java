aab6c4a2507db9832f53f0869a4810a42cd013cd
Gurchetan <gurchetan9966@gmail.com>
2025-03-19 09:20:58 +0530

    commit and push

--- TestIgnore.java
+++ TestIgnore.java
@@ -1,6 +1,6 @@
 public class TestIgnore {
     public static void main(String[] args) {
-        System.out.println("Testing .gitignore for .class files!");
+        System.out.println("Testing .gitignore hi .class files!");
     }
 }
 
