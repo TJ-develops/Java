7df11a67d842529c2ca991e3a8e9c2d3a9f87485
Gurchetan <gurchetan9966@gmail.com>
2025-03-19 14:54:59 +0530

    commit push

---
+++ Java Basics/add.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class add {
+public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int firstnum = input.nextInt();
+        System.out.println("Enter another Number:");
+        int seconfnum = input.nextInt();
+        int sum = firstnum + seconfnum;
+        System.out.println("Sum of your numbers is " +sum);
+        input.close();
+    }
+}
--- Java Basics/userinput.java
+++ Java Basics/userinput.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 public class userinput {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         System.out.print("What's your name?");
+        Scanner input = new Scanner(System.in);
 String name = input.nextLine();
         System.out.println("Get a Life " + name);
         input.close();
