bbc084ddb63a5af72ef9f0c3e890142f24abe639
Gurchetan <gurchetan9966@gmail.com>
2025-03-20 15:53:03 +0530

    byyee

---
+++ Java Basics/shorthand.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public interface shorthand {
+    public static void main(String[] args) {
+        
+        try (Scanner input = new Scanner(System.in)) {
+            int x1 = 2;
+        
+            System.out.println("Enter an Integer:");
+            int x = input.nextInt();
+
+            x *= x1;
+            
+            System.out.println(x);
+        }
+    }
+}
